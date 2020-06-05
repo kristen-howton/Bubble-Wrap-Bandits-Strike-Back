470ddc12b7eb9631e170854971938198568ebe31
unknown <buckleyderekp@gmail.com>
2020-06-05 14:26:56 -0500

    added rolodex

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Bubble_Wrap_Bandits_Strike_Back
 {
@@ -6,7 +7,48 @@ namespace Bubble_Wrap_Bandits_Strike_Back
     {
         static void Main(string[] args)
         {
-            Hacker Hack = new Hacker();
+            Hacker Hack = new Hacker()
+            {
+                Name = "Jon Durr",
+                SkillLevel = 80,
+
+            };
+            Hacker C_Sharp = new Hacker()
+            {
+                Name = "Crystal Elsey",
+                SkillLevel = 82,
+
+            };
+            LockSpecialist YourNameIsTooLong = new LockSpecialist()
+            {
+                Name = "Brant Pippin",
+                SkillLevel = 85,
+
+            };
+            LockSpecialist BeardedBandit = new LockSpecialist()
+            {
+                Name = "Derek Buckley",
+                SkillLevel = 79,
+
+            };
+            Muscle BonnieK = new Muscle()
+            {
+                Name = "Kristen Howton",
+                SkillLevel = 97,
+
+            };
+
+            List<IRobber> rolodex = new List<IRobber>
+            {
+
+                Hack,
+                C_Sharp,
+                YourNameIsTooLong,
+                BeardedBandit,
+                BonnieK
+            };
+
+            Console.WriteLine($" The Number of operatives in your rolodex is {rolodex.Count}!!!!!");
 
         }
     }
